3f054641d1739bf9122eff5a2a5a8e7cbcf5910d
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-07-31 18:57:29 +0200

    Testing.

--- create_ia_tables.py
+++ create_ia_tables.py
@@ -15,11 +15,13 @@ import func_fabrik_01_groupcreate
 import func_fabrik_02_formcreate
 import func_fabrik_03_formgroup
 import func_fabrik_04_listcreate
+import func_fabrik_05_element
 
 """
 INDEX
 ENVIRONMENT
-ASSIGNMENT CATEGORY
+CREATE ASSIGNMENT CATEGORY
+CREATE FINDING LIKELIHOOD
 CLOSING
 """
 
@@ -29,17 +31,20 @@ ENVIRONMENT
 
 # DECLARE VARIABLES
 l_drop_table: bool = True
-l_assignment_category: bool = True
+l_assignment_category: bool = False
+l_finding_likelihood: bool = True
 s_sql: str = ""
+
+if func_configure.l_debug_project:
+    print("CREATE IA TABLES")
+    print("----------------")
+    print("OPEN DATABASE")
 
 # OPEN THE LOG WRITER
 if func_configure.l_log_project:
     func_file.write_log("Now")
-    func_file.write_log("SCRIPT: CREATE STANDARD TABLES")
-    func_file.write_log("------------------------------")
-
-if func_configure.l_debug_project:
-    print("OPEN DATABASE")
+    func_file.write_log("SCRIPT: CREATE IA TABLES")
+    func_file.write_log("------------------------")
 
 # INPUT THE DATABASE NAME
 print("")
@@ -61,7 +66,7 @@ curs.execute("START TRANSACTION;")
 curs.execute("SET time_zone = '+00:00';")
 
 """*************************************************************************
-ASSIGNMENT CATEGORY
+CREATE ASSIGNMENT CATEGORY
 *************************************************************************"""
 
 if l_assignment_category:
@@ -70,6 +75,7 @@ if l_assignment_category:
         print("BUILD ASSIGNMENT CATEGORY TABLE")
 
     # DEFINE VARIABLES
+    s_name: str = "Assignment Categories 3"
     s_table: str = func_configure.s_joomla_prefix + "_aa_assignment_category"
     i_group: int = 0
     i_form: int = 0
@@ -79,12 +85,12 @@ if l_assignment_category:
     # DROP AND CREATE TABLE
     if l_drop_table:
         if func_configure.l_debug_project:
-            print("DROP TABLE")
+            print("DROP TABLE " + s_table)
         curs.execute("DROP TABLE IF EXISTS `" + s_table + "`;")
 
     # CREATE TABLE
     if func_configure.l_debug_project:
-        print("CREATE TABLE")
+        print("CREATE TABLE " + s_table)
     s_sql = "CREATE TABLE IF NOT EXISTS `%TABLE%` (" \
             "`id` int(11) NOT NULL, " \
             "`name` varchar(50) DEFAULT NULL, " \
@@ -104,7 +110,7 @@ if l_assignment_category:
     # INSERT DEFAULT RECORDS
     if l_drop_table:
         if func_configure.l_debug_project:
-            print("INSERT VALUES")
+            print("INSERT VALUES " + s_table)
         s_sql = "INSERT INTO `%TABLE%` " \
                 "(`id`, `name`, `description`, `active`, `private`, `from`, `to`, `created`, `created_by`)" \
                 " VALUES " \
@@ -133,40 +139,143 @@ if l_assignment_category:
         curs.execute(s_sql)
 
         # CALL THE GROUP CREATE FUNCTION
-        try:
-            i_group = func_fabrik_01_groupcreate.fabrik_group_create(False, "Assignment category 2.00")
+        i_group = func_fabrik_01_groupcreate.fabrik_group_create(False, s_name)
+        if func_configure.l_debug_project:
             print("Group: " + str(i_group))
-            if func_configure.l_log_project:
-                func_file.write_log("%t GROUP CREATED: " + str(i_group))
-        except Exception as e:
-            func_system.error_message(e)
+        if func_configure.l_log_project:
+            func_file.write_log("%t GROUP CREATED: " + str(i_group))
 
         # CALL THE FORM CREATE FUNCTION
-        try:
-            i_form = func_fabrik_02_formcreate.fabrik_form_create(False, "Assignment category 2.00")
+        i_form = func_fabrik_02_formcreate.fabrik_form_create(False, s_name)
+        if func_configure.l_debug_project:
             print("Form: " + str(i_form))
-            if func_configure.l_log_project:
-                func_file.write_log("%t FORM CREATED: " + str(i_form))
-        except Exception as e:
-            func_system.error_message(e)
+        if func_configure.l_log_project:
+            func_file.write_log("%t FORM CREATED: " + str(i_form))
 
         # CALL THE FORM GROUP CREATE FUNCTION
-        try:
-            i_form_group = func_fabrik_03_formgroup.fabrik_form_group(False, str(i_group), str(i_form))
+        i_form_group = func_fabrik_03_formgroup.fabrik_form_group(False, str(i_group), str(i_form))
+        if func_configure.l_debug_project:
             print("Form group: " + str(i_form_group))
-            if func_configure.l_log_project:
-                func_file.write_log("%t FORM GROUP CREATED: " + str(i_form_group))
-        except Exception as e:
-            func_system.error_message(e)
+        if func_configure.l_log_project:
+            func_file.write_log("%t FORM GROUP CREATED: " + str(i_form_group))
 
         # CALL THE LIST CREATE FUNCTION
-        try:
-            i_list = func_fabrik_04_listcreate.fabrik_list_create(False, "Assignment category 2.00", str(i_form), s_table, "id")
+        i_list = func_fabrik_04_listcreate.fabrik_list_create(False, str(i_form), s_table, "id", s_name)
+        if func_configure.l_debug_project:
             print("List: " + str(i_list))
-            if func_configure.l_log_project:
-                func_file.write_log("%t LIST CREATED: " + str(i_list))
-        except Exception as e:
-            func_system.error_message(e)
+        if func_configure.l_log_project:
+            func_file.write_log("%t LIST CREATED: " + str(i_list))
+
+        func_fabrik_05_element.fabrik_element_create(False, False, "1", str(i_group), "1", "id", "ID")
+        func_fabrik_05_element.fabrik_element_create(False, False, "2", str(i_group), "2", "created", "Date created")
+
+    # COMMIT
+    mysql_connection.commit()
+
+
+"""*************************************************************************
+CREATE FINDING LIKELIHOOD
+*************************************************************************"""
+
+if l_finding_likelihood:
+
+    if func_configure.l_debug_project:
+        print("BUILD FINDING LIKELIHOOD TABLE")
+
+    # DEFINE VARIABLES
+    s_name: str = "Finding Likelihood"
+    s_table: str = func_configure.s_joomla_prefix + "_finding_likelihood"
+    i_group: int = 0
+    i_form: int = 0
+    i_form_group: int = 0
+    i_list: int = 0
+
+    # DROP AND CREATE TABLE
+    if l_drop_table:
+        if func_configure.l_debug_project:
+            print("DROP TABLE " + s_table)
+        curs.execute("DROP TABLE IF EXISTS `" + s_table + "`;")
+
+    # CREATE TABLE
+    if func_configure.l_debug_project:
+        print("CREATE TABLE " + s_table)
+    s_sql = "CREATE TABLE IF NOT EXISTS `%TABLE%` (" \
+            "`ia_findlike_auto` int(11) NOT NULL, " \
+            "`name` varchar(50) DEFAULT NULL, " \
+            "`description` text, " \
+            "`active` text, " \
+            "`private` text, " \
+            "`from` datetime DEFAULT NULL, " \
+            "`to` datetime DEFAULT NULL, " \
+            "`created` datetime DEFAULT NULL, " \
+            "`created_by` int(11) DEFAULT NULL, " \
+            "`modified` datetime DEFAULT NULL, " \
+            "`modified_by` int(11) DEFAULT NULL " \
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table to store assignment categories';"
+    s_sql = s_sql.replace("%TABLE%", s_table)
+    curs.execute(s_sql)
+
+    # INSERT DEFAULT RECORDS
+    if l_drop_table:
+        if func_configure.l_debug_project:
+            print("INSERT VALUES " + s_table)
+        s_sql = "INSERT INTO `%TABLE%` " \
+                "(`id`, `name`, `description`, `active`, `private`, `from`, `to`, `created`, `created_by`)" \
+                " VALUES " \
+                "(1, 'ADMINISTRATION', 'Office administration.', '1', '0', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%), " \
+                "(2, 'ASSIGNMENT', 'Audit assignments.', '1', '0', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%), " \
+                "(3, 'CONSULTATION', 'Consultation work.', '1', '0', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%), " \
+                "(4, 'DEVELOPMENT', 'Software development.', '1', '0', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%), " \
+                "(5, 'ELECTION', 'Election audits.', '1', '0', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%), " \
+                "(6, 'PRIVATE', 'Private work.', '1', '1', '%NOW%', '2099-12-31', '%NOW%', %CREATED_BY%);"
+        s_sql = s_sql.replace("%TABLE%", s_table)
+        s_sql = s_sql.replace("%NOW%", datetime.datetime.now().strftime("%Y%m%d"))
+        s_sql = s_sql.replace("%CREATED_BY%", func_configure.s_user_id)
+        curs.execute(s_sql)
+        mysql_connection.commit()
+        if func_configure.l_log_project:
+            func_file.write_log("%t INSERT RECORDS: " + s_table)
+
+        # ADD INDEXES
+        s_sql = "ALTER TABLE `%TABLE%` ADD PRIMARY KEY (`id`);"
+        s_sql = s_sql.replace("%TABLE%", s_table)
+        curs.execute(s_sql)
+
+        # ADD AUTO_INCREMENT
+        s_sql = "ALTER TABLE `%TABLE%` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;"
+        s_sql = s_sql.replace("%TABLE%", s_table)
+        curs.execute(s_sql)
+
+        # CALL THE GROUP CREATE FUNCTION
+        i_group = func_fabrik_01_groupcreate.fabrik_group_create(False, s_name)
+        if func_configure.l_debug_project:
+            print("Group: " + str(i_group))
+        if func_configure.l_log_project:
+            func_file.write_log("%t GROUP CREATED: " + str(i_group))
+
+        # CALL THE FORM CREATE FUNCTION
+        i_form = func_fabrik_02_formcreate.fabrik_form_create(False, s_name)
+        if func_configure.l_debug_project:
+            print("Form: " + str(i_form))
+        if func_configure.l_log_project:
+            func_file.write_log("%t FORM CREATED: " + str(i_form))
+
+        # CALL THE FORM GROUP CREATE FUNCTION
+        i_form_group = func_fabrik_03_formgroup.fabrik_form_group(False, str(i_group), str(i_form))
+        if func_configure.l_debug_project:
+            print("Form group: " + str(i_form_group))
+        if func_configure.l_log_project:
+            func_file.write_log("%t FORM GROUP CREATED: " + str(i_form_group))
+
+        # CALL THE LIST CREATE FUNCTION
+        i_list = func_fabrik_04_listcreate.fabrik_list_create(False, str(i_form), s_table, "id", s_name)
+        if func_configure.l_debug_project:
+            print("List: " + str(i_list))
+        if func_configure.l_log_project:
+            func_file.write_log("%t LIST CREATED: " + str(i_list))
+
+        func_fabrik_05_element.fabrik_element_create(False, False, "1", str(i_group), "1", "id", "ID")
+        func_fabrik_05_element.fabrik_element_create(False, False, "2", str(i_group), "2", "created", "Date created")
 
     # COMMIT
     mysql_connection.commit()
