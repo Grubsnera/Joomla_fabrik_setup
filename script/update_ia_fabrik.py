6b26cda87c7bd31af46fc1a6c11659a30e4d3b1e
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-08-08 16:10:08 +0200

    Add finding master update.

--- script/update_ia_fabrik.py
+++ script/update_ia_fabrik.py
@@ -19,6 +19,7 @@ from _fabrik_form_modules import func_goback_button
 # INDEX
 """
 ENVIRONMENT
+UPDATE FINDING MASTER
 UPDATE FINDING LIKELIHOODS
 UPDATE FINDING CONTROL VALUES
 END
@@ -40,8 +41,67 @@ if func_configure.l_log_project:
 
 # DECLARE VARIABLES
 mysql_cxn = func_mysql.mysql_open(False, func_configure.s_joomla_database)
-l_finding_likelihood: bool = True
-l_finding_control_value: bool = True
+l_finding_master: bool = True
+l_finding_likelihood: bool = False
+l_finding_control_value: bool = False
+
+"""****************************************************************************
+UPDATE FINDING MASTER
+****************************************************************************"""
+
+if l_finding_master:
+
+    # UPDATE LOG
+    if func_configure.l_debug_project:
+        print("UPDATE FINDING MASTER")
+        print("---------------------")
+
+    if func_configure.l_log_project:
+        func_file.write_log('UPDATE FINDING MASTER')
+        func_file.write_log('---------------------')
+
+    # UPDATE LIST
+    if func_configure.l_log_project:
+        func_file.write_log('%t UPDATE LIST: Finding Master')
+
+    # UPDATE FORM
+    if func_configure.l_log_project:
+        func_file.write_log('%t UPDATE FORM: Finding Master')
+    s_table = func_mysql.get_fabrik_table(False, 'form')
+    func_goback_button.update_goback_button(mysql_cxn, s_table, 'Finding Master', '1')
+
+    # UPDATE GROUP
+    if func_configure.l_log_project:
+        func_file.write_log('%t UPDATE GROUP: Finding Master')
+    s_table = func_mysql.get_fabrik_table(False, 'group')
+    func_label.update_label(mysql_cxn, s_table, 'Finding Master', 'Finding')
+
+    # UPDATE ELEMENTS
+    if func_configure.l_log_project:
+        func_file.write_log('%t UPDATE ELEMENTS:Finding Master')
+    s_table = func_mysql.get_fabrik_table(False, 'element')
+
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_auto', 'Finding ID')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_date', 'Finding date')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_name', 'Audit finding')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_note', 'Personal notes')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_desc', 'Finding description')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_createdate', 'Created')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_createby', 'Created by')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_editdate', 'Modified')
+    func_label.update_label(mysql_cxn, s_table, 'ia_find_editby', 'Modified by')
+
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_auto', 0)
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_desc', 0)
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_createdate', 0)
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_createby', 0)
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_editdate', 0)
+    func_showinlist.update_show_in_list(mysql_cxn, s_table, 'ia_find_editby', 0)
+
+    func_hidden.update_hidden(mysql_cxn, s_table, 'ia_find_createdate', 1)
+    func_hidden.update_hidden(mysql_cxn, s_table, 'ia_find_createby', 1)
+    func_hidden.update_hidden(mysql_cxn, s_table, 'ia_find_editdate', 1)
+    func_hidden.update_hidden(mysql_cxn, s_table, 'ia_find_editby', 1)
 
 """****************************************************************************
 UPDATE FINDING LIKELIHOODS
